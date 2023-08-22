44903d65151eda35965dcfec57ced875c7378d30
Chris Dahlin <chrisdahlin@gmail.com>
2023-08-22 15:26:10 -0400

    Added Strip and Title functions

--- python-CS50/hello.py
+++ python-CS50/hello.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove white space from name and captitalize
+name = name.strip().title()
+
+# Say hello to the user
+print(f"Hello, {name}")
