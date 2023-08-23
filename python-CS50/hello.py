38c062f1c321ab942db895e73d48f4914345cd89
Chris Dahlin <chrisdahlin@gmail.com>
2023-08-22 20:23:46 -0400

    Calculator and updated Hello app

--- python-CS50/hello.py
+++ python-CS50/hello.py
@@ -1,18 +1,15 @@
-import time
+# # Ask user for their name
+# name = input("What's your name? ")
 
-# Ask user for their name
+# # Remove white space from name and captitalize
+# name = name.strip().title()
+
+# # Say hello to the user
+# print(f"Hello, {name}")
+def hello(to="World"):
+    print("Hello,", to)
+
+
+hello()
 name = input("What's your name? ")
-
-# Remove white space from name and captitalize
-name = name.strip().title()
-
-# Say hello to the user
-print(f"Hello, {name}")
-
-time.sleep(1)
-
-x = int(input("What's X? "))
-
-y = int(input("What's Y? "))
-
-print(x + y)
+hello(name)
