251ef326ca9d0118e114292e833912551e278706
Chris Dahlin <chrisdahlin@gmail.com>
2023-08-28 21:05:06 -0400

    Updated latest number.py

--- python-CS50/number.py
+++ python-CS50/number.py
@@ -1,37 +1,17 @@
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
 
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            return int(input("What's x? "))
+            return int(input(prompt))
         except ValueError:
-            print("x is not an integer")
+            pass
 
 
 main()
-
-
-# Adds prompt
-
-
-# def main():
-#     x = get_int("What's x? ")
-#     print(f"x is {x}")
-
-
-# def get_int(prompt):
-#     while True:
-#         try:
-#             return int(input(prompt))
-#         except ValueError:
-#             pass
-
-
-# main()
-
 
 # Adds pass
 
