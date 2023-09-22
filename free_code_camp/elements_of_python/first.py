6d8cf616f82facb5ba62b0b91a1b243d7d8439ee
Chris Dahlin <chrisdahlin@gmail.com>
2023-09-22 11:35:52 -0400

    Python - Function/Def classwork

---
+++ free_code_camp/elements_of_python/first.py
@@ -0,0 +1,24 @@
+# def song_lyrics():
+#     print("I'm a lumberjack, and I'm okay.")
+#     print('I sleep all night and I work all day.')
+
+
+# print("Yo")
+# x = 5
+# print(x)
+# song_lyrics()
+
+def greet(lang):
+    if lang == 'es':
+        print('Hola')
+    elif lang == 'fr':
+        print('Bonjour')
+    else:
+        print('Hello')
+
+
+greet('en')
+greet('es')
+greet('fr')
+
+language = greet(input('Enter your language: '))
