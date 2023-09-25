28c68560380ca625dc503fa302342af252305d5a
Chris Dahlin <chrisdahlin@gmail.com>
2023-09-25 16:31:28 -0400

    CS50 Python - Plates solution and Dict progress

--- python-CS50/Classwork/plates.py
+++ python-CS50/Classwork/plates.py
@@ -1,91 +1,3 @@
-# def main():
-#     plate = input("Plate: ")
-#     if is_valid(plate):
-#         print("Valid")
-#     else:
-#         print("Invalid")
-
-
-# def is_valid(s):
-#     # Plate can only contain a max of 6 characters ( Numbers or Letters)
-#     # Minimum of 2 characters
-
-#     if len(s) < 2 or len(s) > 6:
-#         return False
-
-#     # Start with 2 characters
-#     if s[0].isalpha() == False or s[1].isalpha() == False:
-#         return False
-
-#     # Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate;
-#     # AAA22A would not be acceptable.
-#     # The first number used cannot be a ‘0’.
-
-#     i = 0
-#     while 1 < len(s):
-#         if s[i].isalpha() == False:
-#             if s[i] == '0':
-#                 return False
-#             else:
-#                 break
-#         i += 1
-
-#     # No periods, spaces, or punctuation marks are allowed.
-
-#     for c in s:
-#         if c in ['.', ' ', '!', '?']:
-#             return False
-
-#     # If all test pass, return True
-#     return True
-
-# main()
-
-
-# def main():
-#     plate = input("Plate: ")
-#     if is_valid(plate):
-#         print("Valid")
-#     else:
-#         print("Invalid")
-
-
-# def is_valid(s):
-#     # Plate can only contain a max of 6 characters ( Numbers or Letters)
-#     # Minimum of 2 characters
-
-#     if len(s) < 2 or len(s) > 6:
-#         return False
-
-#     # Start with 2 characters
-#     if not s[0].isalpha() or not s[1].isalpha():
-#         return False
-
-#     # Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate;
-#     # AAA22A would not be acceptable.
-#     # The first number used cannot be a ‘0’.
-
-#     i = 2  # Start checking from the 3rd character
-#     while i < len(s):
-#         if not s[i].isalpha():
-#             if s[i] == '0' and i == 2:
-#                 return False
-#             elif not s[i].isdigit():
-#                 return False
-#         i += 1
-
-#     # No periods, spaces, or punctuation marks are allowed.
-
-#     for c in s:
-#         if c in ['.', ' ', '!', '?']:
-#             return False
-
-#     # If all tests pass, return True
-#     return True
-
-
-# main()
-
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -95,37 +7,18 @@ def main():
 
 
 def is_valid(s):
-    # Plate can only contain a max of 6 characters (Numbers or Letters)
-    # Minimum of 2 characters
+    if len(s) >=2 and len(s) <=6:
+        if s.isalpha():
+            return True
+        elif s.isalnum() and s[0:2].isalpha():
+            for char in s:
+                if char.isdigit():
+                    position = s.index(char)
 
-    if len(s) < 2 or len(s) > 6:
-        return False
+                    if s[position:].isdigit() and int(char) != 0:
+                        return True
 
-    # Start with 2 characters
-    if not s[0].isalpha() or not s[1].isalpha():
-        return False
-
-    # Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable vanity plate;
-    # AAA22A would not be acceptable.
-    # The first number used cannot be a ‘0’.
-
-    i = 2  # Start checking from the 3rd character
-    while i < len(s):
-        if not s[i].isalpha():
-            if s[i] == '0' and i == 2:
-                return False
-            elif not s[i].isdigit():
-                return False
-        i += 1
-
-    # No periods, spaces, or punctuation marks are allowed.
-
-    for c in s:
-        if c in ['.', ' ', '!', '?']:
-            return False
-
-    # If all tests pass, return True
-    return True
-
+                    else:
+                        return False
 
 main()
--- w3schools/python/dict.py
+++ w3schools/python/dict.py
@@ -1,8 +1,31 @@
-thisdict =	{
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+# thisdict =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = thisdict["model"]
+# y = str(thisdict["year"])
+# print("Ford " + x + " " + y)
+
+
+# z = thisdict.keys()
+# print(z)
+
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
 }
-x = thisdict["model"]
-y = str(thisdict["year"])
-print("Ford " + x + " " + y)
+
+x = car.keys()
+
+print(x) #before the change
+
+car["color"] = "white"
+car["weight"] = "1000kg"
+
+print(x) #after the change
+
+c = car["weight"]
+print(c)
