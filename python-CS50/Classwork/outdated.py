0d2ea49e4c69925ce2bb69d3d7d88caa878e2c6c
Chris Dahlin <chrisdahlin@gmail.com>
2023-09-26 14:35:28 -0400

    CS50 Python - Outdated solution

--- python-CS50/Classwork/outdated.py
+++ python-CS50/Classwork/outdated.py
@@ -14,26 +14,53 @@ months = [
 ]
 
 while True:
-    date = input("Date: ")
-    try:
-        month, day, year = date.split("/")
+    date = input("Date: ").strip()
 
-        if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+    try:
+        m, d, y = date.split("/")
+        if (int(m) > 0 and int(m) < 13) and (int(d) > 0 and int(d) < 32):
             break
+
     except:
         try:
-            old_month, old_day, year = date.split(" ")
+            alt_m, alt_d, y = date.split(" ")
+            if "," in alt_d:
+                for month in range(len(months)):
+                    if alt_m == months[month]:
+                        m = month + 1
 
-            for i in range(len(months)):
-                if old_month == months[i]:
-                    month = i + 1
+            d = alt_d.replace(",", "")
 
-            day = old_day.replace(",", "")
-            if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+            if (int(m) > 0 and int(m) < 13) and (int(d) > 0 and int(d) < 32):
                 break
-
         except:
             print()
             pass
 
-print(f"{year}-{int(month):02}-{int(day):02}")
+print(f"{y}-{int(m):02}-{int(d):02}")
+
+
+# while True:
+#     date = input("Date: ")
+#     try:
+#          month, day, year = date.split("/")
+
+#          if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+#             break
+#     except:
+#         try:
+#             old_month, old_day, year = date.split(" ")
+
+#             for i in range(len(months)):
+#                 if old_month == months[i]:
+#                     month = i + 1
+
+#             day = old_day.replace(",","")
+#             if (int(month) >= 1 and int(month) <= 12) and (int(day) >= 1 and int(day) <= 31):
+#               break
+
+#         except:
+#             print()
+#             pass
+
+# print(f"{year}-{int(month):02}-{int(day):02}")
