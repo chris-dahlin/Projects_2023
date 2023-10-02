3da3fe6dd9531898964dc5f10af133d010b2cc7e
Chris Dahlin <chrisdahlin@gmail.com>
2023-10-02 18:46:22 -0400

    Think Python - Do Twice function

--- think_python/functions_exercises.py
+++ think_python/functions_exercises.py
@@ -1,6 +1,27 @@
-def right_justify(s):
-    print(s.rjust(70))
+# def right_justify(s):
+#     print(s.rjust(70))
 
 
-right_justify('monty')
-right_justify('Chris')
+# right_justify('monty')
+# right_justify('Chris')
+
+
+def two_twice(f):
+    f()
+    f()
+
+
+def do_twice(f, arg):
+    f(arg)
+    f(arg)
+
+
+do_twice(print, 'spam')
+# do_twice(print, 'spam', 'eggs')
+
+
+def print_spam():
+    print('spam')
+
+
+two_twice(print_spam)
