a2c55082154cfacccbaf3e3b74000532dd5cb633
Chris Dahlin <chrisdahlin@gmail.com>
2023-11-30 17:12:58 -0500

    Module 1 Practice

--- Python/python_institute/module1/module1.py
+++ Python/python_institute/module1/module1.py
@@ -3,3 +3,16 @@ print(5+5)
 print("The itsy bitsy spider\nclimbed up the waterspout.")
 print("\\")
 print("Down came the rain\nand washed the spider out.")
+
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+
+#  My_name_is*Monty*Python.*
+
+
+print("Programming","Essentials","in", sep= "***", end="...")
+print("Python")
