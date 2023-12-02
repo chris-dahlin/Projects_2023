de01c0cd60bc698953d35f1452c88045eca35bc2
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-01 19:52:02 -0500

    Module 2 - progress

--- Python/python_institute/module2/module2.py
+++ Python/python_institute/module2/module2.py
@@ -1,10 +1,38 @@
-print(111_111_111)
+# print(111_111_111)
 
-print(0o123)
+# print(0o123)
 
-print(0x123)
+# print(0x123)
 
-print(True > False)
-print(True < False)
+# print(True > False)
+# print(True < False)
 
-print('"I\'m" \n ""learning"" \n """Python"""')
+# print('"I\'m" \n ""learning"" \n """Python"""')
+
+
+# print(2 * 3)
+# print(2 * 3.)
+# print(2. * 3)
+# print(2. * 3.)
+
+
+# print(6 / 3)
+# print(6 / 3.)
+# print(6. / 3)
+# print(6. / 3.)
+
+# print(6 // 3)
+# print(6 // 3.)
+# print(6. // 3)
+# print(6. // 3.)
+
+# print(6 // 4)
+# print(6. // 4)
+
+# print(6 / 4)
+# print(6. / 4)
+
+# print(-6 // 4)
+# print(6. // -4)
+
+print(12 % 4.5)
