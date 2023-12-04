6346d9c9a8b001645f8d4cf27f1a0df040f8d372
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-04 10:55:42 -0500

    Module 2 - progress

--- Python/python_institute/module2/module2.py
+++ Python/python_institute/module2/module2.py
@@ -45,9 +45,15 @@
 
 # print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
 
-print((2 ** 4), (2 * 4.), (2 * 4))
+# print((2 ** 4), (2 * 4.), (2 * 4))
 
-print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+# print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
 
-print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+# print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+
+# Off Limit keywords for variables
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+
 
