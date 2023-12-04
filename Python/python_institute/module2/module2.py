b53c49826fe26c60c6bd8b9d09cd1a22381e527e
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-04 10:58:10 -0500

    Module 2 - progress

--- Python/python_institute/module2/module2.py
+++ Python/python_institute/module2/module2.py
@@ -54,6 +54,11 @@
 # Off Limit keywords for variables
 # ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 
+# var = 1
+# account_balance = 1000.0
+# client_name = 'John Doe'
+# print(var, account_balance, client_name)
+# print(var)
 
-
-
+# var = "3.8.5"
+# print("Python version: " + var)
