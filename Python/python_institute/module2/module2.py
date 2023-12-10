6f4d2a44392145f8379b9163aebb4d407c8f95d6
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-10 15:13:16 -0500

    Module 2 - Progress

--- Python/python_institute/module2/module2.py
+++ Python/python_institute/module2/module2.py
@@ -105,7 +105,10 @@ a = 2 # number of hours
 seconds = 3600 # number of seconds in 1 hour
 
 print("Hours: ", a) #printing the number of hours
-print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
-
+# print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+print("Seconds in Hours: ", a * seconds)
 #here we should also print "Goodbye", but a programmer didn't have time to write any code
-#this is the end of the program that computes the number of seconds in 3 hour
+print("Goodbye")
+#this is the end of the program that computes the number of seconds in 3 hours
+a = 3
+print("Seconds in Hours: ", a * seconds)
