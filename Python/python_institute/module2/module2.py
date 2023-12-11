7089c1dc36a356e05069b9f01d6b9fc5ef71c907
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-11 14:56:03 -0500

    Module 2 - Complete

--- Python/python_institute/module2/module2.py
+++ Python/python_institute/module2/module2.py
@@ -161,9 +161,33 @@
 
 # print("\nThat's all, folks!")
 
-hour = int(input("Starting time (hours): "))
-mins = int(input("Starting time (minutes): "))
-dura = int(input("Event duration (minutes): "))
+# hour = int(input("Starting time (hours): "))
+# mins = int(input("Starting time (minutes): "))
+# dura = int(input("Event duration (minutes): "))
 
-# Write your code here.
-print((hour+(mins+dura)//60)%24,":",(mins+dura)%60)
+# # Write your code here.
+# print((hour+(mins+dura)//60)%24,":",(mins+dura)%60)
+
+# name = input("Enter your name: ")
+# print("Hello, " + name + ". Nice to meet you!")
+
+# print("\nPress Enter to end the program.")
+# input()
+# print("THE END.")
+
+# num_1 = input("Enter the first number: ") # Enter 12
+# num_2 = input("Enter the second number: ") # Enter 21
+
+# print(num_1 + " " + num_2)
+
+# x = int(input("Enter a number: ")) # The user enters 2
+# print(x * "5")
+
+x = 11
+y = 4
+
+x = x % y
+x = x % y
+y = y % x
+
+print(y)
