743ebbc41d4b716e9a3cb11e784d5621ddbd6b97
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-16 15:08:28 -0500

    Module 3 - Progress

--- Python/python_institute/module2/module3/module3.py
+++ Python/python_institute/module2/module3/module3.py
@@ -4,8 +4,29 @@
 # var = 1  # Assigning 1 to var
 # print(var == 0)
 
-var = 0  # Assigning 0 to var
-print(var != 0)
+# var = 0  # Assigning 0 to var
+# print(var != 0)
 
-var = 1  # Assigning 1 to var
-print(var != 0)
+# var = 1  # Assigning 1 to var
+# print(var != 0)
+
+# n = int(input("Enter a number: "))
+# print(n >= 100)
+
+# if true_or_false_condition:
+#     perform_if_condition_true
+# else:
+#     perform_if_condition_false
+
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
