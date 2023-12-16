d1affd138066945ab8f04a1cc2fcd880eba52f4a
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-16 15:22:38 -0500

    Module 3 - progress

--- Python/python_institute/module3/module3.py
+++ Python/python_institute/module3/module3.py
@@ -76,15 +76,22 @@
 # Read three numbers.
 
 
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-number3 = int(input("Enter the third number: "))
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+# number3 = int(input("Enter the third number: "))
 
-# Check which one of the numbers is the greatest
-# and pass it to the largest_number variable.
+# # Check which one of the numbers is the greatest
+# # and pass it to the largest_number variable.
 
-largest_number = max(number1, number2, number3)
+# largest_number = max(number1, number2, number3)
 
-# Print the result.
-print("The largest number is:", largest_number)
+# # Print the result.
+# print("The largest number is:", largest_number)
+
+flower = input("What is your favorite flower?")
+
+if flower == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+else:
+    print("No, I want a big Spathiphyllum!")
 
