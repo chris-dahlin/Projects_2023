37433b961be94076b951b18500e13978e1305fab
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-16 15:15:25 -0500

    Module 3 - Progress

--- Python/python_institute/module3/module3.py
+++ Python/python_institute/module3/module3.py
@@ -19,14 +19,72 @@
 #     perform_if_condition_false
 
 # Read two numbers
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+
+# # Choose the larger number
+# if number1 > number2:
+#     larger_number = number1
+# else:
+#     larger_number = number2
+
+# # Print the result
+# print("The larger number is:", larger_number)
+
+# # Read two numbers
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+
+# # Choose the larger number
+# if number1 > number2: larger_number = number1
+# else: larger_number = number2
+
+# # Print the result
+# print("The larger number is:", larger_number)
+
+# # Read three numbers
+# number1 = int(input("Enter the first number: "))
+# number2 = int(input("Enter the second number: "))
+# number3 = int(input("Enter the third number: "))
+
+# # We temporarily assume that the first number
+# # is the largest one.
+# # We will verify this soon.
+# largest_number = number1
+
+# # We check if the second number is larger than current largest_number
+# # and update largest_number if needed.
+# if number2 > largest_number:
+#     largest_number = number2
+
+# # We check if the third number is larger than current largest_number
+# # and update largest_number if needed.
+# if number3 > largest_number:
+#     largest_number = number3
+
+# # Print the result
+# print("The largest number is:", largest_number)
+
+# largest_number = -999999999
+# number = int(input())
+# if number == -1:
+#     print(largest_number)
+#     exit()
+# if number > largest_number:
+#     largest_number = number
+# # Go to line 02
+# Read three numbers.
+
+
 number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
 
-# Choose the larger number
-if number1 > number2:
-    larger_number = number1
-else:
-    larger_number = number2
+# Check which one of the numbers is the greatest
+# and pass it to the largest_number variable.
 
-# Print the result
-print("The larger number is:", larger_number)
+largest_number = max(number1, number2, number3)
+
+# Print the result.
+print("The largest number is:", largest_number)
+
