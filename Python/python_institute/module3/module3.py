e5dc076107a073e75c1852cb4a20b4c58ba3869a
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-25 22:46:47 -0500

    Module 3 - Progress

--- Python/python_institute/module3/module3.py
+++ Python/python_institute/module3/module3.py
@@ -184,45 +184,61 @@
 # and counts how many numbers are even and how many are odd.
 # The program terminates when zero is entered.
 
-odd_numbers = 0
-even_numbers = 0
+# odd_numbers = 0
+# even_numbers = 0
 
-# Read the first number.
-number = int(input("Enter a number or type 0 to stop: "))
+# # Read the first number.
+# number = int(input("Enter a number or type 0 to stop: "))
 
-# 0 terminates execution.
-while number != 0:
-    # Check if the number is odd.
-    if number % 2 == 1:
-        # Increase the odd_numbers counter.
-        odd_numbers += 1
-    else:
-        # Increase the even_numbers counter.
-        even_numbers += 1
-    # Read the next number.
-    number = int(input("Enter a number or type 0 to stop: "))
+# # 0 terminates execution.
+# while number != 0:
+#     # Check if the number is odd.
+#     if number % 2 == 1:
+#         # Increase the odd_numbers counter.
+#         odd_numbers += 1
+#     else:
+#         # Increase the even_numbers counter.
+#         even_numbers += 1
+#     # Read the next number.
+#     number = int(input("Enter a number or type 0 to stop: "))
 
-# Print results.
-print("Odd numbers count:", odd_numbers)
-print("Even numbers count:", even_numbers)
+# # Print results.
+# print("Odd numbers count:", odd_numbers)
+# print("Even numbers count:", even_numbers)
 
 
-# break - example
+# # break - example
 
-print("The break instruction:")
-for i in range(1, 6):
-    if i == 3:
+# print("The break instruction:")
+# for i in range(1, 6):
+#     if i == 3:
+#         break
+#     print("Inside the loop.", i)
+# print("Outside the loop.")
+
+
+# # continue - example
+
+# print("\nThe continue instruction:")
+# for i in range(1, 6):
+#     if i == 3:
+#         continue
+#     print("Inside the loop.", i)
+# print("Outside the loop.")
+
+largest_number = -99999999
+counter = 0
+
+while True:
+    number = int(input("Enter a number or type -1 to end program: "))
+    if number == -1:
         break
-    print("Inside the loop.", i)
-print("Outside the loop.")
+    counter += 1
+    if number > largest_number:
+        largest_number = number
 
-
-# continue - example
-
-print("\nThe continue instruction:")
-for i in range(1, 6):
-    if i == 3:
-        continue
-    print("Inside the loop.", i)
-print("Outside the loop.")
+if counter != 0:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
 
